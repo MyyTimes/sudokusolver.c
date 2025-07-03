7344fa6a32df52d9c97a402b1859b3a6cbe1eaab
Muhammed Yusuf <77261036+MyyTimes@users.noreply.github.com>
2025-07-03 13:57:50 +0300

    Almost complete classic sudoku solver

---
+++ ColorPaletteForTerminal.h
@@ -0,0 +1,24 @@
+#ifndef COLOR_PALETTE_FOR_TERMINAL_H
+#define COLOR_PALETTE_FOR_TERMINAL_H
+
+#define RESET   "\033[0m" 
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+
+//BOLD COLORS
+#define BBLACK   "\033[1;30m"
+#define BRED     "\033[1;31m"
+#define BGREEN   "\033[1;32m"
+#define BYELLOW  "\033[1;33m"
+#define BBLUE    "\033[1;34m"
+#define BMAGENTA "\033[1;35m"
+#define BCYAN    "\033[1;36m"
+#define BWHITE   "\033[1;37m"
+
+#define BackGround "\033[48;5;236m"
+
+#endif // COLOR_PALETTE_FOR_TERMINAL_H
---
+++ SudokuSolver.c
@@ -0,0 +1,428 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "ColorPaletteForTerminal.h"
+
+#define INPUT_SUDOKU "sudoku.txt"
+#define SAVE_STEPS "SaveSteps.txt"
+#define MAX_LINE_LENGTH 100
+#define MARKED_SLOT -1
+
+void FillTempBoard(int**, int, int, int, int);
+void FillBoardWithMarkers(int**, int, int, int, int, int, int);
+int FindProperSlots(int**, int**, int, int, int, int);
+int FindProperInBox(int**, int, int, int, int, int*, int*);
+int FindProperInLine(int**, int, int, int, int*, int*); /* HORIZANTAL and VERTICAL */
+
+int **CreateBoard(FILE*, int*, int*, int*);
+int CheckBoard(int**, int, int, int);
+void PrintBoard(int**, int, int, int);
+void CopyBoard(int**, int**, int);
+void FreeBoard(int**, int);
+void SaveBoardToText(FILE*, int**, int, int);
+
+int IsValidPlacement(int **board, int boardLength, int boxW, int boxH, int row, int col, int num)
+{
+    int i, j;
+    int startRow = row - (row % boxH);
+    int startCol = col - (col % boxW);
+
+    for (i = 0; i < boardLength; i++) {
+        if (board[row][i] == num || board[i][col] == num)
+            return 0;
+    }
+
+    for (i = startRow; i < startRow + boxH; i++) {
+        for (j = startCol; j < startCol + boxW; j++) {
+            if (board[i][j] == num)
+                return 0;
+        }
+    }
+
+    return 1;
+}
+
+
+int main()
+{
+    FILE *sudokuFile = fopen(INPUT_SUDOKU, "r");
+    FILE *saveStepsFile = fopen(SAVE_STEPS, "w");
+    if(sudokuFile == NULL || saveStepsFile == NULL)
+    {
+        printf(RED "%s does not exist!\n" RESET, INPUT_SUDOKU);
+        return 1;
+    }
+
+    int num, i, isCompleted = 0;
+    int boardLength, boxWidth, boxHeight;
+    int **tempBoard, **board = CreateBoard(sudokuFile, &boardLength, &boxWidth, &boxHeight);
+
+    tempBoard = (int**)calloc(boardLength, sizeof(int*));
+    for(i = 0; i < boardLength; i++)
+    {
+        tempBoard[i] = (int*)calloc(boardLength, sizeof(int));
+    }
+
+    PrintBoard(board, boardLength, boxWidth, boxHeight);
+
+    if(CheckBoard(board, boardLength, boxWidth, boxHeight))
+    {
+        printf(RED "This sudoku board cannot be solvable!\n" RESET);
+        return 1;
+    }
+
+    while(!isCompleted)
+    {
+        isCompleted = 1;
+        for(num = 1; num <= boardLength; num++) /* try each possible numbers */
+        {
+            CopyBoard(board, tempBoard, boardLength);
+            FillTempBoard(tempBoard, boardLength, boxWidth, boxHeight, num);
+
+            /* SAVE board steps */
+            SaveBoardToText(saveStepsFile, board, boardLength, num);
+            SaveBoardToText(saveStepsFile, tempBoard, boardLength, num);
+
+            if(FindProperSlots(board, tempBoard, boardLength, boxWidth, boxHeight, num))
+            {
+                isCompleted = 0; /* any of these number could not be located */
+            }
+        }
+    }
+
+    printf(GREEN "\nSOLVED SUDOKU:\n" RESET);
+    PrintBoard(board, boardLength, boxWidth, boxHeight);
+
+    printf("Board solved: ");
+    if(CheckBoard(board, boardLength, boxWidth, boxHeight))
+        printf(RED "Unsuccessful\n" RESET);
+    else
+        printf(GREEN "Successful\n" RESET);
+    
+    FreeBoard(board, boardLength);
+    FreeBoard(tempBoard, boardLength);
+
+    fclose(sudokuFile);
+    fclose(saveStepsFile);
+    return 0;
+}
+
+void SaveBoardToText(FILE *file, int **board, int length, int num)
+{
+    int i, j;
+    fprintf(file, "Number: %d\n", num);
+    for(i = 0; i < length; i++)
+    {
+        for(j = 0; j < length; j++)
+        {
+            fprintf(file, "%2d ", board[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+    fprintf(file, "\n");
+}
+
+int FindProperSlots(int **board, int **tempBoard, int boardLength, int boxW, int boxH, int num)
+{
+    int isFound, foundRow, foundCol, isLocated = 0;
+    int i, j;
+
+    /* IN EACH BOXES */
+    for(i = 0; i < boardLength; i += boxH)
+    {
+        for(j = 0; j < boardLength; j += boxW)
+        {
+            if(FindProperInBox(tempBoard, i, j, boxW, boxH, &foundRow, &foundCol))
+            {
+                board[foundRow][foundCol] = num;
+                isLocated = 1;
+                return isLocated;
+            }
+        }
+    }
+
+    /* HORIZONTAL */
+    for(i = 0; i < boardLength; i++) 
+    {
+        if (FindProperInLine(tempBoard, boardLength, i, 1, &foundRow, &foundCol))
+        {
+            board[foundRow][foundCol] = num;
+            isLocated = 1;
+            return isLocated;
+        }
+    }
+
+    /* VERTICAL */
+    for(i = 0; i < boardLength; i++) 
+    {
+        if (FindProperInLine(tempBoard, boardLength, i, 0, &foundRow, &foundCol)) 
+        {
+            board[foundRow][foundCol] = num;
+            isLocated = 1;
+            return isLocated;
+        }
+    }
+
+    return isLocated;
+}
+
+int FindProperInLine(int **tempBoard, int boardLength, int fixedIndex, int isRow, int *foundRow, int *foundCol)
+{
+    int i, row, col;
+    int isFound = 0;
+    for(i = 0; i < boardLength; i++) 
+    {
+        row = isRow ? fixedIndex : i;
+        col = isRow ? i : fixedIndex;
+
+        if(tempBoard[row][col] == 0) 
+        {
+            if(isFound == 1) 
+                return 0; /* more than one empty slot */
+            else
+            {
+                isFound = 1;
+                /* save pos */
+                *foundRow = row;
+                *foundCol = col;
+            }
+        }
+    }
+
+    return isFound;
+}
+
+
+int FindProperInBox(int **tempBoard, int row, int col, int boxW, int boxH, int *foundRow, int *foundCol)
+{
+    int isFound = 0, i, j;
+    for(i = 0; i < boxH; i++)
+    {
+        for(j = 0; j < boxW; j++)
+        {
+            if(tempBoard[row + i][col + j] == 0)
+            {
+                if(isFound == 1)
+                    return 0; /* more than one empty slot */
+                else
+                {
+                    isFound = 1;
+                    /* save pos */
+                    *foundRow = row + i;
+                    *foundCol = col + j;
+                }
+            }
+        }
+    }
+
+    return isFound;
+}
+
+void FillTempBoard(int **markedBoard, int boardLength, int boxW, int boxH, int num)
+{
+    int row, col, i, j;
+
+    /* FILL THE BOARD WITH '-1' */
+    for(row = 0; row < boardLength; row++)
+    {
+        for(col = 0; col < boardLength; col++)
+        {
+            if(markedBoard[row][col] == num)
+            {
+                FillBoardWithMarkers(markedBoard, boardLength, boxW, boxH, row, col, num);
+            }
+        }
+    }    
+}
+
+void FillBoardWithMarkers(int **markedBoard, int boardLength, int boxW, int boxH, int row, int col, int num)
+{
+    int i, j, startRow, startCol;
+
+    /* FILL ROW */
+    for(i = 0; i < boardLength; i++)
+    {
+        if(markedBoard[row][i] != num)
+            markedBoard[row][i] = MARKED_SLOT;
+    }
+
+    /* FILL COLUMN */
+    for(i = 0; i < boardLength; i++)
+    {
+        if(markedBoard[i][col] != num)
+            markedBoard[i][col] = MARKED_SLOT;
+    }
+
+    /* CHECK BOX */
+    startRow = row - (row % boxH);
+    startCol = col - (col % boxW);
+    for(i = startRow; i < startRow + boxH; i++)
+    {
+        for(j = startCol; j < startCol + boxW; j++)
+        {
+            if(markedBoard[i][j] != num)
+                markedBoard[i][j] = MARKED_SLOT;
+        }
+    }
+}
+
+int CheckBoard(int **board, int boardLength, int boxW, int boxH)
+{
+    int row, col, i, j;
+    int startCol, startRow;
+
+    for(row = 0; row < boardLength; row++)
+    {
+        for(col = 0; col < boardLength; col++)
+        {
+            if(board[row][col] == 0) continue;
+
+            /* CHECK ROW */
+            for(i = 0; i < boardLength; i++)
+            {
+                if(board[row][col] == board[row][i] && col != i)
+                {
+                    printf("The number %d is the same in (%d, %d) and (%d, %d)!\n", board[row][col], row, col, row, i);
+                    return 1;
+                }
+            }
+            /* CHECK COLUMN */
+            for(i = 0; i < boardLength; i++)
+            {
+                if(board[row][col] == board[i][col] && row != i)
+                {
+                    printf("The number %d is the same in (%d, %d) and (%d, %d)!\n", board[row][col], row, col, i, col);
+                    return 1;
+                }
+            }
+            /* CHECK BOX */
+            startRow = row - (row % boxH);
+            startCol = col - (col % boxW);
+            for(i = startRow; i < startRow + boxH; i++)
+            {
+                for(j = startCol; j < startCol + boxW; j++)
+                {
+                    if(board[row][col] == board[i][j] && !(row == i && col == j))
+                    {
+                        printf("The number %d is the same in (%d, %d) and (%d, %d)!\n", board[row][col], row, col, i, j);
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+int **CreateBoard(FILE *file, int *boardLength, int *boxWidth, int *boxHeight)
+{
+    int **board;
+    int col, row;
+    char line[MAX_LINE_LENGTH];
+    char *token;
+
+    fgets(line, sizeof(line), file);
+    sscanf(line, " %d %d %d", boardLength, boxHeight, boxWidth);
+
+    board = (int**)calloc(*boardLength, sizeof(int*));
+
+    row = 0;
+    while(fgets(line, sizeof(line), file) && row < *boardLength)
+    {
+        board[row] = (int*)calloc(*boardLength, sizeof(int));
+        line[strcspn(line, "\n")] = '\0';
+
+        /* PARSE ROW */
+        col = 0;
+        char *start = line;
+        char *ptr = line;
+
+        /* CHANGE THIS PART */
+        while (*ptr) {
+            if (*ptr == ',') {
+                // Virgül ile biten token
+                if (start == ptr) {
+                    // Arada veri yok, boş hücre
+                    board[row][col++] = 0;
+                } else {
+                    // Veri var, kopyala ve ata
+                    char temp = *ptr;
+                    *ptr = '\0';
+                    board[row][col++] = atoi(start);
+                    *ptr = temp;
+                }
+                start = ptr + 1;
+            }
+            ptr++;
+        }
+        // Son token (virgülden sonra ya da satır sonunda)
+        if (start == ptr) {
+            board[row][col++] = 0;
+        } else {
+            board[row][col++] = atoi(start);
+        }
+
+        row++;
+    }
+
+    return board;
+}
+
+void PrintBoard(int **board, int boardLength, int boxW, int boxH)
+{
+    int row, col;
+
+    for(row = 0; row < boardLength; row++)
+    {  
+        if(row % boxH == 0) /* PRINT LINE TO SEPERATE BOXES */
+        {
+            for(col = 0; col < boardLength * 2 + (boardLength / boxW); col++)
+            {
+                if(col % (boxW * 2 + 1) == 0) printf(BMAGENTA "." RESET);
+                else printf(BYELLOW "-" RESET);
+            }
+            printf(BMAGENTA ".\n" RESET);
+        }
+        
+        for(col = 0; col < boardLength; col++)
+        {
+            if(col % boxW == 0) printf(BYELLOW "|" RESET);
+
+            if(board[row][col] == 0) printf("  ");
+            else
+            {
+                printf("%d ", board[row][col]);
+            }
+        }
+        printf(BYELLOW "|\n" RESET);
+    }
+
+    /* LAST LINE */
+    for(col = 0; col < boardLength * 2 + (boardLength / boxW); col++)
+    {
+        if(col % (boxW * 2 + 1) == 0) printf(BMAGENTA "." RESET);
+        else printf(BYELLOW "-" RESET);
+    }
+    printf(BMAGENTA ".\n" RESET);
+}
+
+void CopyBoard(int **board, int **tempBoard, int boardLength)
+{
+    int i, j;
+    for(i = 0; i < boardLength; i++)
+        for(j = 0; j < boardLength; j++)
+            tempBoard[i][j] = board[i][j];
+}
+
+void FreeBoard(int **board, int size)
+{
+    int i;
+    for(i = 0; i < size; i++)
+    {
+        free(board[i]);
+    }
+    free(board);
+}
+
+
